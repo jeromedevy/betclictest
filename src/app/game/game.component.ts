30a69cff1e6c84f0218c1d4a31f53783c304b7e6
DEVY Jerome <jdevy.ext@orange.com>
2019-01-14 13:05:30 +0100

    ajoute computer choice

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-game',
@@ -8,12 +9,17 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class GameComponent implements OnInit {
 
-  player : string;
+  player: string;
+  initList =  [{ url: "./assets/img/scissors_64.png", name: "scissors", show: true }, { url: "./assets/img/paper_64.png", name: "paper", show: true }, { url: "./assets/img/rock_64.png", name: "rock", show: true }];
+  imagesList = [];
+  computerChoice: object;
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.player = this.route.snapshot.paramMap.get('name');
+    this.imagesList =  [ ...this.initList ].map(item=>({...item}));
+    this.computerChoice = {url: "#", name: "", show: false};
     console.log("the player : " + this.player);
   }
 
@@ -21,4 +27,24 @@ export class GameComponent implements OnInit {
     this.router.navigate(['/player']);
   }
 
+  async selectObject(selectedObject) {
+    this.computerChoice = this.initList[this.randomIntInc(0,2)];
+    console.log("select : " + selectedObject);
+    this.imagesList.forEach(image => {
+      if (image.name != selectedObject) {
+        image.show = false;
+      }
+    });
+    await this.delay(1500)
+    this.ngOnInit();
+
+  }
+
+  private delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  private randomIntInc(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low)
+  }
 }
